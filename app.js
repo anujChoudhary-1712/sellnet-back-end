22da165111be487642b0743151fba78b0d53b276
anuj <anuj00000010@gmail.com>
2024-04-28 22:00:03 +0530

    updated cors

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ dbConnect()
 app.use(express.json())
 app.use(
     cors({
-      origin: ["http://localhost:3000"],
+      origin: ["http://localhost:3000","https://creatify-zeta.vercel.app"],
       credentials:true,
       optionsSuccessStatus: 200,
     })
